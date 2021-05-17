b6b3ac9fec02af1d8bebf7ff9374424d1d5e3f7a
Bianca Del Carretto <bianca.delcarretto@hudl.com>
2021-05-17 15:53:17 +0200

    Fix webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,10 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.ts'),
+  output: {
+    path: __dirname,
+    filename: 'js/main.js',
+  },
   module: {
     rules: [
       {
